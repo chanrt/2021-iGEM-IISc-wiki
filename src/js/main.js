b2dc0e7d95656c34db82e130f8fc9b88f8e50d21
Chandan Relekar <61446958+chanrt@users.noreply.github.com>
2021-07-23 00:08:14 +0530

    Basic model up and running

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
-console.log('JS works!')
-
 // reset iGEM
 if (window.location.href.includes('igem.org')) {
     var ids = ['HQ_page', 'content', 'bodyContent', 'mw-content-text']
     for (var i = 0; i < ids.length; i++)
         document.querySelector('#' + ids[i]).removeAttribute('id');
     var classes = ['mw-content-ltr']
-    for (var i = 0; i < classes.length; i++)
-    {
+    for (var i = 0; i < classes.length; i++) {
         var elements = document.querySelectorAll('.' + classes[i]);
-        for (var j = 0; j < elements.length; j++)
-        {
+        for (var j = 0; j < elements.length; j++) {
             elements[j].classList.remove(classes[i]);
         }
     }
